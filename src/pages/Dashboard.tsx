af353ff5df068253068e8e8505b9af76e049a31c
abrambagus <you@example.com>
2022-01-01 18:56:53 +0700

    41-commit

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import Wrapper from '../components/Wrapper';
+
+export default class Dashboard extends Component {
+  render() {
+    return <Wrapper>Dashboard</Wrapper>;
+  }
+}
