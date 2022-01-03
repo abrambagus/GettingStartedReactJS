4065578767058c0d456960914554bde0ffe76502
abrambagus <you@example.com>
2022-01-03 16:27:09 +0700

    57-commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import './App.css';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import RoleCreate from './pages/roles/RoleCreate';
+import RoleEdit from './pages/roles/RoleEdit';
+import Roles from './pages/roles/Roles';
+import UserCreate from './pages/users/UserCreate';
+import UserEdit from './pages/users/UserEdit';
 import Users from './pages/users/Users';
 
 function App() {
@@ -11,9 +16,14 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Route path="/" exact component={Dashboard} />
-        <Route path="/users" component={Users} />
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
+        <Route path="/users" exact component={Users} />
+        <Route path="/users/create" component={UserCreate} />
+        <Route path="/users/:id/edit" component={UserEdit} />
+        <Route path="/roles" exact component={Roles} />
+        <Route path="/roles/create" component={RoleCreate} />
+        <Route path="/roles/:id/edit" component={RoleEdit} />
       </BrowserRouter>
     </div>
   );
